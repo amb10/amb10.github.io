ce83c6673a35373b8e4d4d2196b3dae102732288
abutlercei <abutler@ceiamerica.com>
2025-07-23 14:33:11 -0400

    Adding Home page

--- angular-version/src/app/app.routes.ts
+++ angular-version/src/app/app.routes.ts
@@ -1,12 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
-import { About } from './about/about';
 import { Services } from './services/services';
 import { Contact } from './contact/contact';
 
 export const routes: Routes = [
   { path: '', component: Home, title: 'Lauren Keltz Styling' },
-  { path: 'about', component: About, title: 'About' },
   { path: 'services', component: Services, title: 'Services' },
   { path: 'contact', component: Contact, title: 'Contact' },
   // { path: '**', component: PageNotFoundComponent },
--- angular-version/src/app/navigation-bar/navigation-bar.ts
+++ angular-version/src/app/navigation-bar/navigation-bar.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MatIconRegistry, MatIconModule } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navigation-bar',
-  imports: [MatIconModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './navigation-bar.html',
   styleUrl: './navigation-bar.css',
 })
 export class NavigationBar {
+  isMenuOpen = false;
+
   constructor(
     private matIconRegistry: MatIconRegistry,
     private domSanitizer: DomSanitizer
@@ -20,5 +23,9 @@ export class NavigationBar {
         'assets/icons/hair-salon-icon.svg'
       )
     );
+  }
+
+  onMenuClick() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 }
