b5cd552f9506b8c8e73cd7ab263fa2ca91aa054c
abutlercei <abutler@ceiamerica.com>
2025-07-24 10:44:06 -0400

    Adding Contact page

--- angular-version/src/app/contact/contact.ts
+++ angular-version/src/app/contact/contact.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import {
+  faFacebook,
+  faInstagram,
+  faThreads,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [MatIconModule, FontAwesomeModule],
   templateUrl: './contact.html',
-  styleUrl: './contact.css'
+  styleUrl: './contact.css',
 })
 export class Contact {
-
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faFacebook, faInstagram, faThreads);
+  }
 }
